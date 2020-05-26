df5e9a68558acd012029aba68e8eaa7dd866eff1
Michael Steenbeek <m.o.steenbeek@gmail.com>
2020-05-26 23:03:51 +0200

    Initial version

---
+++ src/DatHeader.php
@@ -0,0 +1,63 @@
+<?php
+namespace RCTPHP;
+
+use RuntimeException;
+
+/**
+ * Class DatHeader
+ *
+ * Reads the header of an RCT2 .DAT object file and saves its metadata.
+ */
+class DatHeader
+{
+    public const OBJECT_TYPE_RIDE = 0;
+    public const OBJECT_TYPE_SMALL_SCENERY = 1;
+    public const OBJECT_TYPE_LARGE_SCENERY = 2;
+    public const OBJECT_TYPE_WALLS = 3;
+    public const OBJECT_TYPE_BANNERS = 4;
+    public const OBJECT_TYPE_PATHS = 5;
+    public const OBJECT_TYPE_PATH_BITS = 6;
+    public const OBJECT_TYPE_SCENERY_GROUP = 7;
+    public const OBJECT_TYPE_PARK_ENTRANCE = 8;
+    public const OBJECT_TYPE_WATER = 9;
+    public const OBJECT_TYPE_SCENARIO_TEXT = 10;
+
+    // Folders as used by the objexport tool
+    public const TYPE_TO_FOLDER = [
+        "ride",
+        "scenery_small",
+        "scenery_large",
+        "scenery_wall",
+        "footpath_banner",
+        "footpath",
+        "footpath_item",
+        "scenery_group",
+        "park_entrance",
+        "water",
+        "", // Scenario text objects are not supposed to be converted
+    ];
+
+    public int $flags;
+    public string $name;
+    public int $checksum;
+
+    public function __construct(string $filename)
+    {
+        $fp = fopen($filename, 'rb');
+        if ($fp === false)
+        {
+            throw new RuntimeException('Could not open file!');
+        }
+
+        $this->flags = unpack('V', (fread($fp, 4)))[1]; // 32-bit little endian
+        $this->name = fread($fp, 8); // ASCII string
+        $this->checksum = unpack('V', (fread($fp, 4)))[1];   // 32-bit little endian
+
+        fclose($fp);
+    }
+
+    public function getType(): int
+    {
+        return $this->flags & 0x0F;
+    }
+}
---
+++ src/DatToJSONConverter.php
@@ -0,0 +1,93 @@
+<?php
+namespace RCTPHP;
+
+use RuntimeException;
+
+/**
+ * Class DatToJSONConverter
+ *
+ * This can be used to convert DAT objects to official ones.
+ *
+ * This was quickly written, and with only my personal use case in mind.
+ * If this ends up being used outside my system, a refactor would be in order.
+ */
+class DatToJSONConverter
+{
+    public const LANGUAGE_DIR = '/home/michael/Programma\\\'s/Code/CLionProjects/Localisation/data/language';
+    public const MAIN_DIR = '/home/michael';
+    public const INPUT_DIR = self::MAIN_DIR . '/ToOfficialObjects';
+    public const OUTPUT_DIR = self::MAIN_DIR . '/ConvertedOfficialObjects';
+    public const OBJEXPORT_PATH = '/home/michael/Programma\\\'s/Code/CLionProjects/objects/tools/objexport/bin/Debug/netcoreapp3.1/objexport';
+    public const OPENRCT2_PATH = '/home/michael/Programma\\\'s/OpenRCT2/openrct2';
+
+    public function __construct()
+    {
+
+    }
+
+    public function doEverything(): void
+    {
+        exec('rm -R ' . self::OUTPUT_DIR . '/official 2> /dev/null');
+        exec('rm -R ' . self::OUTPUT_DIR . '/other 2> /dev/null');
+
+        $inputFiles = $this->datToJson();
+
+        foreach ($inputFiles as $inputFile)
+        {
+            chdir(self::MAIN_DIR);
+
+            if (substr($inputFile, 0, 1) === '.')
+            {
+                continue;
+            }
+
+            $datHeader = new DatHeader(self::INPUT_DIR . '/' . $inputFile);
+
+            $jsonName = str_replace('.dat', '.json', strtolower($inputFile));
+            $oldDir = self::OUTPUT_DIR . '/other/' . DatHeader::TYPE_TO_FOLDER[$datHeader->getType()];
+            $newDir = self::OUTPUT_DIR . '/official/' . DatHeader::TYPE_TO_FOLDER[$datHeader->getType()] . '/official.' . strtolower($datHeader->name);
+            if (!mkdir($newDir, 0777, true) && !is_dir($newDir))
+            {
+                throw new RuntimeException(sprintf('Directory "%s" was not created', $newDir));
+            }
+            $newFile = "$newDir/object.json";
+
+            rename("$oldDir/other.$jsonName", $newFile);
+
+            $images = $this->extractImages($newDir, $datHeader);
+
+            $json = file_get_contents($newFile);
+            $json = preg_replace('/"images": \[.*],/', $images, $json);
+            $json = str_replace('    "objectType"', '    "sourceGame": "official",' . PHP_EOL . '    "objectType"', $json);
+            file_put_contents($newFile, $json);
+        }
+
+        exec('rm -R ' . self::OUTPUT_DIR . '/other');
+    }
+
+    private function datToJson(): array
+    {
+        exec(sprintf("%s %s %s --language %s", self::OBJEXPORT_PATH, self::INPUT_DIR, self::OUTPUT_DIR, self::LANGUAGE_DIR));
+
+        return scandir(self::INPUT_DIR);
+    }
+
+    private function extractImages(string $newDir, DatHeader $datHeader): string
+    {
+        $output = [];
+        $images = '';
+        chdir($newDir);
+        @exec(self::OPENRCT2_PATH . " sprite exportalldat {$datHeader->name} images/ 2> /dev/null", $output);
+        // The output may also include warnings. Just save the actual JSON output.
+        foreach ($output as $outputLine)
+        {
+            if (strpos($outputLine, '{') === 0)
+            {
+                $images .= '        ' . $outputLine . PHP_EOL;
+            }
+        }
+        $images = rtrim($images, ",\n") . PHP_EOL;
+        $images = '"images": [' . PHP_EOL . $images . '    ],';
+        return $images;
+    }
+}
