5514439e2c7a4a63b79ffbff87b888395ce6a123
Michael Steenbeek <m.o.steenbeek@gmail.com>
2022-12-04 21:13:52 +0100

    Refactor DatDataPrinter a bit

--- src/DatDataPrinter.php
+++ src/DatDataPrinter.php
@@ -2,26 +2,44 @@
 namespace RCTPHP;
 
 use RCTPHP\Object\DatHeader;
+use RCTPHP\Object\DATObject;
+use RCTPHP\Object\GenericObject;
 use RCTPHP\Object\SceneryGroupObject;
 
 class DatDataPrinter
 {
     private int $type;
     private string $filename;
+    private ?DATObject $object = null;
 
     public function __construct(string $filename)
     {
         $header = new DatHeader($filename);
         $this->filename = $filename;
         $this->type = $header->getType();
+        $this->read();
     }
 
-    public function printData()
+    private function read(): void
     {
-        if ($this->type === DatHeader::OBJECT_TYPE_SCENERY_GROUP)
+        switch ($this->type)
         {
-            $object = new SceneryGroupObject($this->filename);
-            $object->printData();
+            case DatHeader::OBJECT_TYPE_SCENERY_GROUP:
+                $this->object = new SceneryGroupObject($this->filename);
+                return;
+            default:
+                $this->object = new GenericObject($this->filename);
         }
+    }
+
+    public function printData(): void
+    {
+        if ($this->object === null)
+        {
+            Util::printLn('Onbekend type!');
+            return;
+        }
+
+        $this->object->printData();
     }
 }
