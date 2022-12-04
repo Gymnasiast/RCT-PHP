329ba7b643057e51e6bcff5f5d269b277198d8b0
Michael Steenbeek <m.o.steenbeek@gmail.com>
2022-12-04 21:15:30 +0100

    Extend SceneryGroupObject, make it read priority and entertainer costumes

---
+++ src/Object/GenericObject.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace RCTPHP\Object;
+
+use RCTPHP\Util;
+use RuntimeException;
+use function fclose;
+use function fopen;
+
+final class GenericObject implements DATObject
+{
+    private DatHeader $header;
+
+    public function __construct(string $filename)
+    {
+        $fp = fopen($filename, 'rb');
+        if ($fp === false)
+        {
+            throw new RuntimeException('Could not open file!');
+        }
+
+        $this->header = DatHeader::fromStream($fp);
+        fclose($fp);
+    }
+
+    public function printData(): void
+    {
+        Util::printLn("DAT name: {$this->header->name}");
+        Util::printLn("Flags: {$this->header->flags}");
+        Util::printLn("Checksum: {$this->header->checksum}");
+    }
+}
--- src/Object/SceneryGroupObject.php
+++ src/Object/SceneryGroupObject.php
@@ -16,7 +16,7 @@ use function rewind;
 use const SEEK_CUR;
 use const STR_PAD_LEFT;
 
-class SceneryGroupObject implements StringTableOwner
+class SceneryGroupObject implements DATObject, StringTableOwner
 {
     use StringTableDecoder;
 
@@ -26,6 +26,23 @@ class SceneryGroupObject implements StringTableOwner
     /** @var DatHeader[] */
     public array $objects = [];
     public int $numEntries = 0;
+    public int $priority;
+    // Bitset.
+    private int $entertainerCostumes;
+
+    private const ENTERTAINER_CUSTUMES = [
+        "panda",
+        "tiger",
+        "elephant",
+        "roman",
+        "gorilla",
+        "snowman",
+        "knight",
+        "astronaut",
+        "bandit",
+        "sheriff",
+        "pirate",
+    ];
 
     public function __construct(string $filename)
     {
@@ -46,39 +63,28 @@ class SceneryGroupObject implements StringTableOwner
         fwrite($fp, $rledecoded);
 
         rewind($fp);
-        fseek($fp, 0x111, SEEK_CUR);
+        fseek($fp, 0x10B, SEEK_CUR);
         //        fseek($fp, 6 + (0x80 * 2), SEEK_CUR); // ?
 //        fseek($fp, 8, SEEK_CUR);
 //        $this->numEntries = ord(fread($fp, 1));
 //        fseek($fp, 1, SEEK_CUR);
-//        $priority = ord(fread($fp, 1));
-//        fseek($fp, 1, SEEK_CUR);
-//        $entertainerCostumes = unpack('V', (fread($fp, 4)))[1]; // 32-bit little endian
+        $this->priority = ord(fread($fp, 1));
 
+        fseek($fp, 1, SEEK_CUR);
+        $this->entertainerCostumes = unpack('V', (fread($fp, 4)))[1] >> 4; // 32-bit little endian
 
         $this->readStringTable($fp);
 
-        while (true)
-        {
-            $byte = ord(fread($fp, 1));
-            if ($byte === 0xFF)
-            {
-                break;
-            }
-
-            fseek($fp, -1, SEEK_CUR);
-            $this->objects[] = DatHeader::fromStream($fp);
-        }
-
+        $this->readObjects($fp);
 
 
         fclose($fp);
     }
 
-    public function printData()
+    public function printData(): void
     {
         Util::printLn("DAT name: {$this->header->name}");
-
+        Util::printLn("Priority: {$this->priority}");
 
         foreach ($this->stringTable as $stringTableItem)
         {
@@ -89,8 +95,14 @@ class SceneryGroupObject implements StringTableOwner
         }
 
         Util::printLn('');
+        Util::printLn("Entertainer custumes:");
+        foreach ($this->getEntertainerCostumes() as $costume)
+        {
+            Util::printLn("  {$costume}");
+        }
 
-        $count = count($this->objects);
+        Util::printLn('');
+
         Util::printLn('Objects:');
         Util::printLn('');
 
@@ -103,5 +115,38 @@ class SceneryGroupObject implements StringTableOwner
         }
 
         Util::printLn('');
+    }
+
+    /**
+     * @param resource $fp
+     * @return void
+     */
+    private function readObjects(&$fp): void
+    {
+        while (true)
+        {
+            $byte = ord(fread($fp, 1));
+            if ($byte === 0xFF)
+            {
+                break;
+            }
+
+            fseek($fp, -1, SEEK_CUR);
+            $this->objects[] = DatHeader::fromStream($fp);
+        }
+    }
+
+    public function getEntertainerCostumes(): array
+    {
+        $ret = [];
+        foreach (self::ENTERTAINER_CUSTUMES as $index => $custume)
+        {
+            if ($this->entertainerCostumes & (1 << $index))
+            {
+                $ret[] = $custume;
+            }
+        }
+
+        return $ret;
     }
 }
