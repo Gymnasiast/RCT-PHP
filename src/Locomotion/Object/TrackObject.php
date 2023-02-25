c8dfe207004295c6da87491c65f1fc42dd573e9c
Michael Steenbeek <m.o.steenbeek@gmail.com>
2023-02-25 22:10:15 +0100

    Properly read Locomotion TrackObject

--- src/Locomotion/Object/TrackObject.php
+++ src/Locomotion/Object/TrackObject.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace RCTPHP\Locomotion\Object;
 
+use RCTPHP\Binary;
 use RCTPHP\RCT2\Object\DATObject;
 use RCTPHP\RCT2\Object\StringTableDecoder;
 use RCTPHP\RCT2\Object\StringTableOwner;
@@ -13,6 +14,7 @@ use function file_put_contents;
 use function fopen;
 use function fread;
 use function fseek;
+use function ftell;
 use function fwrite;
 use function rewind;
 use const SEEK_CUR;
@@ -25,6 +27,19 @@ class TrackObject implements DATObject, StringTableOwner
     /** @var RCT2String[][] */
     public array $stringTable = [];
 
+    /** @var DATHeader[] */
+    public array $compatibleTracksRoads = [];
+    /** @var DATHeader[] */
+    public array $mods = [];
+    /** @var DATHeader[] */
+    public array $signals = [];
+    public ?DATHeader $tunnel = null;
+    /** @var DATHeader[] */
+    public array $bridges = [];
+    /** @var DATHeader[] */
+    public array $stations = [];
+
+
     public function __construct($header, string $decoded)
     {
         $this->header = $header;
@@ -32,15 +47,79 @@ class TrackObject implements DATObject, StringTableOwner
         fwrite($fp, $decoded);
 
         rewind($fp);
-        fseek($fp, 0x36, SEEK_CUR);
+        fseek($fp, 0x02, SEEK_CUR); // Space for the name;
+        $trackPieces = Binary::readUint16($fp);
+        $stationTrackPieces = Binary::readUint16($fp);
+        $var06 = Binary::readUint8($fp);
+        $numCompatible = Binary::readUint8($fp);
+        $numMods = Binary::readUint8($fp);
+        $numSignals = Binary::readUint8($fp);
+        $mods = [];
+        for ($i = 0; $i < 4; $i++)
+        {
+            $mods[$i] = Binary::readUint8($fp);
+        }
+        $signals = Binary::readUint16($fp);
+        $compatibleTracks = Binary::readUint16($fp);
+        $compatibleRoads = Binary::readUint16($fp);
+        $buildCostFactor = Binary::readSint16($fp);
+        $sellCostFactor = Binary::readSint16($fp);
+        $tunnelCostFactor = Binary::readSint16($fp);
+        $costIndex = Binary::readUint8($fp);
+        $tunnel = Binary::readUint8($fp);
+        $curveSpeed = Binary::readUint16($fp);
+        $image = Binary::readUint32($fp);
+        $trackObjectFlags = Binary::readUint16($fp);
+        $numBridges = Binary::readUint8($fp);
+        $bridges = [];
+        for ($i = 0; $i < 7; $i++)
+        {
+            $bridges[$i] = Binary::readUint8($fp);
+        }
+        $numStations = Binary::readUint8($fp);
+        $stations = [];
+        for ($i =0; $i < 7; $i++)
+        {
+            $stations[$i] = Binary::readUint8($fp);
+        }
+        $displayOffset = Binary::readUint8($fp);
+        $pad35 = Binary::readUint8($fp);
 
+        if (ftell($fp) !== 0x36)
+        {
+            throw new \Exception('Error in table implementation!');
+        }
         $this->readStringTable($fp);
 
-        // TODO: properly read the object table!
-        fseek($fp, 0x2A0, SEEK_SET);
-        $imageTable = fread($fp, strlen($decoded) - 0x2A0);
+        for ($i = 0; $i < $numCompatible; $i++)
+        {
+            $this->compatibleTracksRoads[] = new DATHeader($fp);
+        }
+
+        for ($i = 0; $i < $numMods; $i++)
+        {
+            $this->mods[] = new DATHeader($fp);
+        }
+
+        for ($i = 0; $i < $numSignals; $i++)
+        {
+            $this->signals[] = new DATHeader($fp);
+        }
+
+        $this->tunnel = new DATHeader($fp);
+
+        for ($i = 0; $i < $numBridges; $i++)
+        {
+            $this->bridges[] = new DATHeader($fp);
+        }
+
+        for ($i = 0; $i < $numStations; $i++)
+        {
+            $this->stations[] = new DATHeader($fp);
+        }
+
+        $imageTable = fread($fp, strlen($decoded) - ftell($fp));
         file_put_contents('imagetable-g0.dat', $imageTable);
-        // imagetable!
 
         fclose($fp);
     }
@@ -50,5 +129,20 @@ class TrackObject implements DATObject, StringTableOwner
         Util::printLn("DAT name: {$this->header->name}");
 
         $this->printStringTables();
+
+        $lists = [
+            'Compatible tracks/roads' => &$this->compatibleTracksRoads,
+            'Mods' => &$this->mods,
+            'Signals' => &$this->signals,
+            'Bridges' => &$this->bridges,
+            'Stations' => &$this->stations,
+        ];
+        foreach ($lists as $key => $list)
+        {
+            $imploded = implode(', ', array_map(static function(DATHeader $header) { return $header->name; }, $list));
+            Util::printLn("$key: {$imploded}");
+        }
+
+        Util::printLn('Tunnel: ' . ($this->tunnel ? $this->tunnel->name : 'N/A'));
     }
 }
